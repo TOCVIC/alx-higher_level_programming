b28eaaa955c2e1065fd0ca701fb9352d19c15238
TOCVIC <86574097+TOCVIC@users.noreply.github.com>
2023-02-17 03:46:59 +0100

    Debugging

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -1,11 +1,12 @@
--- Only comedy
-SELECT tv_shows.title title
+-- lists all shows without the genre Comedy in the database hbtn_0d_tvshows
+-- uses a database to list all rows not linked to one row
+SELECT title
 FROM tv_shows
-JOIN tv_show_genres
-ON tv_shows.id = tv_show_genres.show_id 
-JOIN tv_genres 
-ON tv_genres.id = tv_show_genres.genre_id
-WHERE tv_genres.name NOT LIKE '%Comedy%'
-GROUP BY tv_shows.title
-ORDER BY tv_shows.title ASC;
-
+WHERE title NOT IN
+(SELECT title
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+WHERE tv_genres.name = 'Comedy')
+GROUP BY title
+ORDER BY title ASC;
