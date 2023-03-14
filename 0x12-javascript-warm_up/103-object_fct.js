e2abf66e1976c5feb0723c8222999edd99143b15
TOCVIC <86574097+TOCVIC@users.noreply.github.com>
2023-03-14 22:43:44 +0100

    Update 103-object_fct.js

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -1,10 +1,12 @@
 #!/usr/bin/node
 const myObject = {
-  type: 'object'
+  type: 'object',
   value: 12
 };
 console.log(myObject);
-myObject.incr = () => { myObject.value += 1; };
+myObject.incr = () => { 
+  myObject.value += 1; 
+};
 myObject.incr();
 console.log(myObject);
 myObject.incr();
