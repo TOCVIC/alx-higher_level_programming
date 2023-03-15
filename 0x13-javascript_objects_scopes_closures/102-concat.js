76744596b02ddd5bb7ab8f6ae8da8c1ea0a8941f
TOCVIC <86574097+TOCVIC@users.noreply.github.com>
2023-03-15 03:29:20 +0100

    Add files via upload

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,12 +1,5 @@
 #!/usr/bin/node
-
-const fs = require('fs').promises;
-const { argv } = require('process');
-
-fs.readFile(argv[2], 'utf8')
-  .then(data => fs.writeFile(argv[4], data, 'utf8'))
-  .catch(err => console.error(err));
-
-fs.readFile(argv[3], 'utf8')
-  .then(data => fs.writeFile(argv[4], data, { flag: 'a' }, 'utf8'))
-  .catch(err => console.error(err));
+const fs = require('fs');
+const src1 = fs.readFileSync(process.argv[2], 'utf8');
+const src2 = fs.readFileSync(process.argv[3], 'utf8');
+fs.writeFileSync(process.argv[4], src1 + src2);
