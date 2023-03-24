9ca6cc38bc00b32b895eecc5cea75d90dd2e059b
TOCVIC <vickytochi6882@gmail.com>
2023-03-24 08:44:49 -0700

    debugging

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,17 +1,35 @@
 #!/usr/bin/python3
+
 """
-lists all cities from the database hbtn_0e_4_usa
+    A script that lists all cities from the database hbtn_0e_0_usa
+    Username, password and database names are given as user args
 """
+
 
 import sys
 import MySQLdb
 
-if __name__ == "__main__":
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    c = db.cursor()
-    c.execute("""SELECT cities.id, cities.name, states.name
-                FROM cities
-                JOIN states
-                ON cities.state_id = states.id
-                ORDER BY cities.id ASC""")
-    [print(city) for city in c.fetchall()]
+
+if __name__ == '__main__':
+    db = MySQLdb.connect(user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3],
+                         host='localhost',
+                         port=3306)
+
+    cursor = db.cursor()
+
+    sql = """SELECT c.id, c.name, s.name
+          FROM states s, cities c
+          WHERE c.state_id = s.id
+          ORDER BY c.id ASC"""
+
+    cursor.execute(sql)
+
+    data = cursor.fetchall()
+
+    for row in data:
+        print(row)
+
+    cursor.close()
+    db.close()
