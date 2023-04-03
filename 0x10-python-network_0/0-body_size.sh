365fc6662887fccb7d0fcc4fc7f5fe46efe3895b
TOCVIC <vickytochi6882@gmail.com>
2023-04-03 15:40:02 -0700

    networking

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
-# Takes a URL as an argument, sends a request to that URL, and displays the size of the response body in bytes.
-
-if [[ $# -ne 1 ]]; then
-    echo "Usage: $0 <url>"
-    exit 1
-fi
-
-curl -sI "$1" | grep -i content-length | awk '{print $2}'
+#takes in a URL, sends a request to that URL,and displays the size of the body of the response
+curl -s "$1" | wc -c
